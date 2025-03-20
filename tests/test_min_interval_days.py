af1c792d541bd9c82b98e2bde36346f2eefc0155
carmelly <carmelly@gmail.com>
2025-03-19 23:57:26 -0700

    fix imports

--- tests/test_min_interval_days.py
+++ tests/test_min_interval_days.py
@@ -1,9 +1,5 @@
 import datetime
 import pytest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from models import Peep, Event
 from globals import Globals
 
