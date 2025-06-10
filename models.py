carmelly/peeps_scheduler

commit f09c446c818dddb716ce40202def620306ad1bfb

Support different event durations

models.py
@@ -2,7 +2,7 @@ import datetime
 import random
 import logging
 from enum import Enum
-from constants import DATE_FORMAT, DATESTR_FORMAT
+from constants import ABS_MIN_ROLE, CLASS_CONFIG, DATE_FORMAT, DATESTR_FORMAT
 
 class Role(Enum):
 	LEADER = "Leader"
@@ -142,16 +142,35 @@ class Event:
 	def __init__(self, **kwargs):
 		self.id = kwargs.get("id", 0)
 		self.date = kwargs.get("date", None)
-		self.min_role = kwargs.get("min_role", 5)
-		self.max_role = kwargs.get("max_role", 6)
-		self.attendees = []
-		self.alternates = []
+		
+		self.duration_minutes = kwargs.get("duration_minutes")
+		if self.duration_minutes not in CLASS_CONFIG:
+			raise ValueError(f"Unknown event duration: {self.duration_minutes}")
 
-	def to_dict(self):
-		return {
-			**self.__dict__,
-		}
+		self.attendees = []
+		self.alternates = []	
 	
+	@property
+	def config(self):
+		return CLASS_CONFIG[self.duration_minutes]
+
+	@property
+	def min_role(self):
+		return self.config["min_role"]
+
+	@property
+	def max_role(self):
+		return self.config["max_role"]
+
+	@property
+	def price(self):
+		return self.config["price"]
+	
+	@property
+	def price_per_person(self):
+		num_people = len(self.attendees)
+		return round(self.price / num_people, 0) if num_people else None
+
 	@property
 	def leaders(self):
 		return self.get_attendees_by_role(Role.LEADER)
@@ -168,6 +187,11 @@ class Event:
 	def alt_followers(self):
 		return self.get_alternates_by_role(Role.FOLLOWER)
 
+	def to_dict(self):
+		return {
+			**self.__dict__,
+		}
+
 	def get_attendees_by_role(self, role): 
 		return [p for p in self.attendees if p.role == role]
 	
@@ -181,8 +205,11 @@ class Event:
 		self.alternates.append(peep)
 
 	def is_valid(self):
-		""" Event is valid if we have enough leaders and enough followers to fill the minimum per role """
-		return( len( self.leaders) >= self.min_role and len(self.followers) >= self.min_role)
+		""" Event is valid if we have enough leaders and enough followers to fill the absolute minimum per role """
+		return (
+			len(self.leaders) == len(self.followers)
+			and len(self.leaders) >= ABS_MIN_ROLE
+		)
 	
 	def balance_roles(self):
 		"""
@@ -200,9 +227,23 @@ class Event:
 				alt_peep = larger_group.pop()
 				self.attendees.remove(alt_peep)
 				self.add_alternate(alt_peep)
-		assert len(self.leaders) == len(self.followers) >= self.min_role
+		
+		assert len(self.leaders) == len(self.followers) >= ABS_MIN_ROLE
 		assert len(self.leaders) <= self.max_role
 
+		# Downgrade event duration if needed based on final class size
+		count_per_role = len(self.leaders)
+		if count_per_role < self.min_role: 
+			logging.debug(f"Downgrading Event {self.id} due to underfill ({count_per_role}/role)")
+			for duration in sorted(CLASS_CONFIG.keys()):
+				config = CLASS_CONFIG[duration]
+				if config["min_role"] <= count_per_role <= config["max_role"]:
+					self.duration_minutes = duration
+					break
+			else:
+				logging.warning(f"No valid duration found for balanced count {count_per_role} in event {self.id}.")
+
+		
 	@classmethod
 	def from_dict(cls, data):
 		"""Convert dictionary data back into an Event object."""
@@ -263,15 +304,18 @@ class Event:
 	
 class EventSequence:
 	def __init__(self, events, peeps):
+		# Needed for evaluation
 		self.events = events
 		self.peeps = peeps
+		self.valid_events = []
+
+		# Efficiency metrics 
 		self.num_unique_attendees = 0
 		self.total_attendees = 0
 		self.system_weight = 0
 		self.priority_fulfilled = 0 
 		self.normalized_utilization = 0 
-		self.valid_events = []
-		
+				
 	def validate_alternates(self): 
 		""" Removes alternates that can no longer attend an event due to personal limits """
 		for event in self.valid_events: 
@@ -363,7 +407,7 @@ class EventSequence:
 		)
 		result += f"\t"
 		for event in sorted_events:
-			result += f"\n\t{event}"
+			result += f"\n\t{event}, {event.duration_minutes} mins, ${event.price_per_person:.0f}/person"
 			result += f"\n\t  {event.get_leaders_str()}"
 			result += f"\n\t  {event.get_followers_str()}"
 

constants.py
@@ -1,2 +1,17 @@
 DATE_FORMAT = "%Y-%m-%d %H:%M"
 DATESTR_FORMAT = "%A %B %d - %I%p"
+
+CLASS_CONFIG = {
+	90: {
+		"price": 195.0,
+		"min_role": 4,
+		"max_role": 4,
+	},
+	120: {
+		"price": 260.0,
+		"min_role": 5,
+		"max_role": 8,
+	}
+}
+ABS_MIN_ROLE = min(config["min_role"] for config in CLASS_CONFIG.values())
+ABS_MAX_ROLE = max(config["max_role"] for config in CLASS_CONFIG.values())

scheduler.py
@@ -1,6 +1,7 @@
 import copy
 import logging
 import time
+from constants import ABS_MAX_ROLE, ABS_MIN_ROLE
 from models import Event, EventSequence, Peep, Role
 import utils
 
@@ -10,6 +11,7 @@ class Scheduler:
 		self.max_events = max_events
 		self.output_json = f'data/{data_folder}/output.json'
 		self.result_json = f'data/{data_folder}/results.json'
+		self.target_max = None # max per role used for each run 
 
 	def sanitize_events(self, events, peeps):
 		"""Sanitize events to ensure there are enough leaders and followers to fill roles."""
@@ -19,7 +21,7 @@ class Scheduler:
 			num_leaders = sum(1 for peep in peeps if event.id in peep.availability and peep.role == Role.LEADER)
 			num_followers = sum(1 for peep in peeps if event.id in peep.availability and peep.role == Role.FOLLOWER)
 
-			if num_leaders >= event.min_role and num_followers >= event.min_role:
+			if num_leaders >= ABS_MIN_ROLE and num_followers >= ABS_MIN_ROLE:
 				valid_events.append(event)
 			else:
 				removed_events.append(event)
@@ -29,9 +31,11 @@ class Scheduler:
 	def evaluate_sequence(self, sequence):
 		"Evaluates an event sequence by assigning peeps to events and updating priorities and list order."""
 		for event in sequence.events:
+			effective_max_role = min(event.max_role, self.target_max or event.max_role)
+
 			for peep in sequence.peeps:
 				if peep.can_attend(event):
-					if len(event.get_attendees_by_role(peep.role)) < event.max_role:
+					if len(event.get_attendees_by_role(peep.role)) < effective_max_role:
 						event.add_attendee(peep)
 					else:
 						event.add_alternate(peep)
@@ -159,30 +163,6 @@ class Scheduler:
 			# and s.total_attendees == best_total 
 		]
 
-	def get_sequences_for_class_size(self, og_events, og_peeps, min_role, max_role): 
-		logging.debug(f"Finding best sequences for min of {min_role} and max of {max_role} per role.")
-		
-		# copy the events and peeps list for this iteration
-		events = copy.deepcopy(og_events)
-		peeps = copy.deepcopy(og_peeps)
-		
-		# set class size for all events
-		for event in events:
-			event.min_role = min_role
-			event.max_role = max_role
-		
-		return self.get_valid_sequences(events, peeps)
-
-	def get_valid_sequences(self, events, peeps): 
-		sanitized_events = self.sanitize_events(events, peeps)
-		logging.debug(f"Sanitized Events: {len(sanitized_events)}/{len(events)}")
-
-		if len(sanitized_events) > self.max_events:
-			logging.warning(f"Too many valid events. Trimming to {self.max_events} based on overlap.")
-			sanitized_events = self.remove_high_overlap_events(sanitized_events, peeps, self.max_events)
-
-		return self.evaluate_all_event_sequences(peeps, sanitized_events)
-
 	def run(self, generate_test_data=False, load_from_csv=False):
 		if generate_test_data:
 			logging.info(f"Generating test data and saving to {self.output_json}")
@@ -198,20 +178,26 @@ class Scheduler:
 		logging.debug("Initial Peeps")
 		logging.debug(Peep.peeps_str(peeps))
 
-		# Try events with different min/max per role to get the *actual* best sequence
-		# TODO: make the ultimate min and max configurable
-		# TODO: handle ties and/or show multiple possibilities before choosing one
+		# Get all events that can be filled to the minimum 
+		sanitized_events = self.sanitize_events(events, peeps)
+		logging.debug(f"Sanitized Events: {len(sanitized_events)}/{len(events)}")
+
+		# If too many events, remove some 
+		if len(sanitized_events) > self.max_events:
+			logging.warning(f"Too many valid events. Trimming to {self.max_events} based on overlap.")
+			sanitized_events = self.remove_high_overlap_events(sanitized_events, peeps, self.max_events)
+
+		# Try events with different max per role to get the *actual* best sequence
 		all_sequences = []
-		for min_role in [ 4, 5, 6]: 
-			for max_role in range(min_role, 9):  # up to 8
-				seqs = self.get_sequences_for_class_size(copy.deepcopy(events),copy.deepcopy(peeps), min_role, max_role)
-				all_sequences.extend(seqs)
+		for target_max in range(ABS_MIN_ROLE, ABS_MAX_ROLE + 1):  
+			self.target_max = target_max
+			sequences = self.evaluate_all_event_sequences(peeps, sanitized_events)
+			all_sequences.extend(sequences)
 
 		best = self.get_top_sequences(all_sequences)
 		if not best:
 			logging.info("No sequence could fill any events.")
 			return
-
 
 		if len(best) == 1:
 			best_sequence = best[0]

commit 05db933c18049608d6757578ffa5ecf76ca227b2

Fixes a bug in event.is_valid (introduced in f09c446)

models.py
@@ -206,10 +206,8 @@ class Event:
 
 	def is_valid(self):
 		""" Event is valid if we have enough leaders and enough followers to fill the absolute minimum per role """
-		return (
-			len(self.leaders) == len(self.followers)
-			and len(self.leaders) >= ABS_MIN_ROLE
-		)
+		return( len( self.leaders) >= ABS_MIN_ROLE and len(self.followers) >= ABS_MIN_ROLE)
+
 	
 	def balance_roles(self):
 		"""